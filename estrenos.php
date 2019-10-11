03fc0026122706f02b7f72841fe8925b13246193
David Chacón <davidl.chacong@gmail.com>
2019-10-11 02:06:50 -0400

    :poop: Creando vista estrenos

--- estrenos.php
+++ estrenos.php
@@ -3,22 +3,112 @@
 
   <head>
 
-    <?php include('templates/head.html') ?>
-    <title>Boletox</title>
+    <?php include('templates/head.html')?>
+    <title>Boletox - Estrenos</title>
 
   </head>
 
   <body class="grey darken-4">
 
-    <?php include('templates/header.html') ?>
+    <?php include('templates/header.html')?>
 
     <!-- Inicio del codigo -->
-   
+
+    <br>
+    <div class="container">
+
+        <h2 class="text-center text-light font-weight-bold mb-4">Próximos Estrenos</h2>
+
+        <div class="row">
+
+            <div class="col-lg-3 col-md-12 mb-4">
+
+                <div class="view overlay z-depth-2 mb-2">
+                    <img src="img/covers/LaRebelion.jpg" class="img-fluid rounded" alt="First sample image">
+                    <a>
+                        <div class="mask waves-effect waves-light"></div>
+                    </a>
+                </div>
+                
+                <br>
+                <h4 class="mb-3 font-weight-bold text-light">
+                <strong>La Rebelión</strong>
+                </h4>
+
+                <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae a repellat suscipit culpa quidem voluptate fuga omnis, corporis nisi beatae vel sequi ad voluptatum veritatis natus, voluptates, tenetur earum.</p>
+                <p class="text-light font-weight-bold">Estreno el <strong>15/07/2018</strong></p>
+                
+            </div>
+
+            <div class="col-lg-3 col-md-12 mb-4">
+
+                <div class="view overlay z-depth-2 mb-2">
+                    <img src="img/covers/SpidermanLejosDeCasa.jpg" class="img-fluid rounded" alt="First sample image">
+                    <a>
+                        <div class="mask waves-effect waves-light"></div>
+                    </a>
+                </div>
+                
+                <br>
+                <h4 class="mb-3 font-weight-bold text-light">
+                <strong>Spiderman: Lejos de Casa</strong>
+                </h4>
+
+                <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae a repellat suscipit culpa quidem voluptate fuga omnis, corporis nisi beatae vel sequi ad voluptatum veritatis natus, voluptates, tenetur earum.</p>
+                <p class="text-light font-weight-bold">Estreno el <strong>15/07/2018</strong></p>
+
+                <a class="btn btn-green btn-rounded btn-md waves-effect waves-light">Preventa</a>
+                
+                
+            </div>
+            <div class="col-lg-3 col-md-12 mb-4">
+
+                <div class="view overlay z-depth-2 mb-2">
+                    <img src="img/covers/UglyDolls.jpg" class="img-fluid rounded" alt="First sample image">
+                    <a>
+                        <div class="mask waves-effect waves-light"></div>
+                    </a>
+                </div>
+                
+                <br>
+                <h4 class="mb-3 font-weight-bold text-light">
+                <strong>Ugly Dolls</strong>
+                </h4>
+
+                <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae a repellat suscipit culpa quidem voluptate fuga omnis, corporis nisi beatae vel sequi ad voluptatum veritatis natus, voluptates, tenetur earum.</p>
+                <p class="text-light font-weight-bold">Estreno el <strong>15/07/2018</strong></p>
+                
+                
+            </div>
+
+            <div class="col-lg-3 col-md-12 mb-4">
+
+                <div class="view overlay z-depth-2 mb-2">
+                    <img src="img/covers/MaestrasDelEngaño.jpg" class="img-fluid rounded" alt="First sample image">
+                    <a>
+                        <div class="mask waves-effect waves-light"></div>
+                    </a>
+                </div>
+                
+                <br>
+                <h4 class="mb-3 font-weight-bold text-light">
+                <strong>La Llorona</strong>
+                </h4>
+
+                <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae a repellat suscipit culpa quidem voluptate fuga omnis, corporis nisi beatae vel sequi ad voluptatum veritatis natus, voluptates, tenetur earum.</p>
+                <p class="text-light font-weight-bold">Estreno el <strong>15/07/2018</strong></p>
+                                
+            </div>
+
+        </div>
+
+    </div>
+
     <!-- Final del codigo -->
 
-    <?php include('templates/footer.html') ?>
+    <?php include('templates/footer.html')?>
     <?php include('templates/scripts.html') ?>
-    
+
   </body>
 
 </html>
