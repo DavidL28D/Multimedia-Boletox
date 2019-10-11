23b7b4fb6458e30401ed6a7ad49917d8ac232bec
David Chacón <davidl.chacong@gmail.com>
2019-10-11 01:03:47 -0400

    :poop: Creando vista noticias

--- noticias.php
+++ noticias.php
@@ -11,14 +11,77 @@
   <body class="grey darken-4">
 
     <?php include('templates/header.html') ?>
+    <br>
 
     <!-- Inicio del codigo -->
-   
+
+    <div class="container center">
+
+        <div class="row">
+
+            <div class="col-md-5">
+                <div class="view overlay rounded z-depth-2">
+                    <img src="img/notices/Sala.jpg" alt="Sample image for first version of blog listing" class="img-fluid">
+                    <a>
+                    <div class="mask waves-effect waves-light"></div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-md-7">
+                    
+              <a href="" class="purple-text">
+                  <h6 class="font-weight-bold pb-1">
+                  <i class="fas fa-newspaper"></i> Noticia </h6>
+              </a>
+
+              <h3 class="mb-4 font-weight-bold text-light">
+                  <strong>Alquiler de sala</strong>
+              </h3>
+
+              <p class="text-light">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum nesciunt nam doloribus nobis! Error repellat veritatis consequuntur rem fugit libero voluptas quo, alias numquam ratione corporis consequatur accusamus totam sunt.</p>
+              <a class="btn btn-success btn-md mb-3 waves-effect waves-light">Más detalles</a>
+                         
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+
+            <div class="col-md-5">
+                <div class="view overlay rounded z-depth-2">
+                    <img src="img/notices/Cine2.webp" alt="Sample image for first version of blog listing" class="img-fluid">
+                    <a>
+                    <div class="mask waves-effect waves-light"></div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-md-7">
+                    
+                <a href="" class="purple-text">
+                    <h6 class="font-weight-bold pb-1">
+                    <i class="fas fa-newspaper"></i> Noticia </h6>
+                </a>
+
+                <h3 class="mb-4 font-weight-bold text-light">
+                    <strong>Funciones Especiales</strong>
+                </h3>
+
+                <p class="text-light">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum nesciunt nam doloribus nobis! Error repellat veritatis consequuntur rem fugit libero voluptas quo, alias numquam ratione corporis consequatur accusamus totam sunt.</p>
+                <a class="btn btn-success btn-md mb-3 waves-effect waves-light">Más detalles</a>
+                            
+            </div>
+            
+        </div>
+
+    </div>
+
     <!-- Final del codigo -->
 
+    <br>
     <?php include('templates/footer.html') ?>
     <?php include('templates/scripts.html') ?>
-    
+
   </body>
 
 </html>
