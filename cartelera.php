ec1c74b5d51f9ec64331f1a490348b823bdf1eff
Yeison Fuentes <yeisonf15@gmail.com>
2019-10-11 02:19:42 -0400

    :boom:The Views Billboard and Presale are Ready

--- cartelera.php
+++ cartelera.php
@@ -13,7 +13,187 @@
     <?php include('templates/header.html') ?>
 
     <!-- Inicio del codigo -->
-   
+    <br>
+    <div class="container">
+      <h2 class="text-center text-light font-weight-bold mb-4">Cartelera</h2>
+      <div class="row">
+
+          <div class="col-lg-3 col-md-12 mb-4">
+
+              <div class="view overlay z-depth-2 mb-2">
+                  <img src="img/covers/ToyStory4.jpg" class="img-fluid rounded" alt="First sample image">
+                  <a>
+                      <div class="mask waves-effect waves-light"></div>
+                  </a>
+              </div>
+              
+              <br>
+              <h4 class="mb-3 font-weight-bold text-light">
+              <strong>Toy Story 4</strong>
+              </h4>
+
+              <p class="text-light">Preventa para el <strong>15/07/2018</strong></p>
+              <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae a repellat suscipit culpa quidem voluptate fuga omnis, corporis nisi beatae vel sequi ad voluptatum veritatis natus, voluptates, tenetur earum.</p>
+              
+              <a class="btn btn-green btn-rounded btn-md waves-effect waves-light">Comprar</a>
+              
+          </div>
+
+          <div class="col-lg-3 col-md-12 mb-4">
+
+              <div class="view overlay z-depth-2 mb-2">
+                  <img src="img/covers/LaRebelion.jpg" class="img-fluid rounded" alt="First sample image">
+                  <a>
+                      <div class="mask waves-effect waves-light"></div>
+                  </a>
+              </div>
+              
+              <br>
+              <h4 class="mb-3 font-weight-bold text-light">
+              <strong>La Rebelión</strong>
+              </h4>
+
+              <p class="text-light">Preventa para el <strong>15/07/2018</strong></p>
+              <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae a repellat suscipit culpa quidem voluptate fuga omnis, corporis nisi beatae vel sequi ad voluptatum veritatis natus, voluptates, tenetur earum.</p>
+              
+              <a class="btn btn-green btn-rounded btn-md waves-effect waves-light">Comprar</a>
+              
+          </div>
+          <div class="col-lg-3 col-md-12 mb-4">
+
+              <div class="view overlay z-depth-2 mb-2">
+                  <img src="img/covers/DetectivePikachu.jpg" class="img-fluid rounded" alt="First sample image">
+                  <a>
+                      <div class="mask waves-effect waves-light"></div>
+                  </a>
+              </div>
+              
+              <br>
+              <h4 class="mb-3 font-weight-bold text-light">
+              <strong>Detective Pikachu</strong>
+              </h4>
+
+              <p class="text-light">Preventa para el <strong>15/07/2018</strong></p>
+              <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae a repellat suscipit culpa quidem voluptate fuga omnis, corporis nisi beatae vel sequi ad voluptatum veritatis natus, voluptates, tenetur earum.</p>
+              
+              <a class="btn btn-green btn-rounded btn-md waves-effect waves-light">Comprar</a>
+              
+          </div>
+
+          <div class="col-lg-3 col-md-12 mb-4">
+
+              <div class="view overlay z-depth-2 mb-2">
+                  <img src="img/covers/Anabelle3.jpg" class="img-fluid rounded" alt="First sample image">
+                  <a>
+                      <div class="mask waves-effect waves-light"></div>
+                  </a>
+              </div>
+              
+              <br>
+              <h4 class="mb-3 font-weight-bold text-light">
+              <strong>Anabelle Viene a Casa</strong>
+              </h4>
+
+              <p class="text-light">Preventa para el <strong>15/07/2018</strong></p>
+              <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae a repellat suscipit culpa quidem voluptate fuga omnis, corporis nisi beatae vel sequi ad voluptatum veritatis natus, voluptates, tenetur earum.</p>
+              
+              <a class="btn btn-green btn-rounded btn-md waves-effect waves-light">Comprar</a>
+              
+          </div>
+
+      </div>
+        <div class="row">
+
+            <div class="col-lg-3 col-md-12 mb-4 pb-4">
+
+                <div class="view overlay z-depth-2 mb-2">
+                    <img src="img/covers/Aladdin.jpg" class="img-fluid rounded" alt="First sample image">
+                    <a>
+                        <div class="mask waves-effect waves-light"></div>
+                    </a>
+                </div>
+                
+                <br>
+                <h4 class="mb-3 font-weight-bold text-light">
+                <strong>Aladdin</strong>
+                </h4>
+
+                <p class="text-light">Preventa el <strong>15/07/2018</strong></p>
+                <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae a repellat suscipit culpa quidem voluptate fuga omnis, corporis nisi beatae vel sequi ad voluptatum veritatis natus, voluptates, tenetur earum.</p>
+                
+                <a href="compra.php" class="mt-auto btn btn-green btn-rounded btn-md waves-effect waves-light">Comprar</a>
+
+                
+
+                
+            </div>
+
+            <div class="col-lg-3 col-md-12 mb-4 pb-4">
+
+                <div class="view overlay z-depth-2 mb-2">
+                    <img src="img/covers/AvengersEndgame.jpg" class="img-fluid rounded" alt="First sample image">
+                    <a>
+                        <div class="mask waves-effect waves-light"></div>
+                    </a>
+                </div>
+                
+                <br>
+                <h4 class="mb-3 font-weight-bold text-light">
+                <strong>Avengers: Endgame</strong>
+                </h4>
+
+                <p class="text-light">Preventa el <strong>15/07/2018</strong></p>
+                <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae a repellat suscipit culpa quidem voluptate fuga omnis, corporis nisi beatae vel sequi ad voluptatum veritatis natus, voluptates, tenetur earum.</p>
+                
+                <a href="compra.php" class="btn btn-green btn-rounded btn-md waves-effect waves-light">Comprar</a>
+                
+            </div>
+            <div class="col-lg-3 col-md-12 mb-4 pb-4">
+
+                <div class="view overlay z-depth-2 mb-2 ">
+                    <img src="img/covers/CementerioMaldito.jpg" class="img-fluid rounded" alt="First sample image">
+                    <a>
+                        <div class="mask waves-effect waves-light"></div>
+                    </a>
+                </div>
+                
+                <br>
+                <h4 class="mb-3 font-weight-bold text-light">
+                <strong>Cementerio Maldito</strong>
+                </h4>
+
+                <p class="text-light">Preventa el <strong>15/07/2018</strong></p>
+                <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae a repellat suscipit culpa quidem voluptate fuga omnis, corporis nisi beatae vel sequi ad voluptatum veritatis natus, voluptates, tenetur earum.</p>
+                
+                <a href="compra.php" class="btn btn-green btn-rounded btn-md waves-effect waves-light">Comprar</a>
+                
+            </div>
+
+            <div class="col-lg-3 col-md-12 mb-4 pb-4">
+
+                <div class="view overlay z-depth-2 mb-2">
+                    <img src="img/covers/LaLlorona.jpg" class="img-fluid rounded" alt="First sample image">
+                    <a>
+                        <div class="mask waves-effect waves-light"></div>
+                    </a>
+                </div>
+                
+                <br>
+                <h4 class="mb-3 font-weight-bold text-light">
+                <strong>La Llorona</strong>
+                </h4>
+
+                <p class="text-light">Preventa el <strong>15/07/2018</strong></p>
+                <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita molestiae a repellat suscipit culpa quidem voluptate fuga omnis, corporis nisi beatae vel sequi ad voluptatum veritatis natus, voluptates, tenetur earum.</p>
+                
+                <a href="compra.php" class="btn btn-green btn-rounded btn-md waves-effect waves-light">Comprar</a>
+                
+            </div>
+
+        </div>
+
+        
+    </div>
     <!-- Final del codigo -->
 
     <?php include('templates/footer.html') ?>
